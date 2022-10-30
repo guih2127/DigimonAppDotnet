95c2777445dc41409368d4184ac171aa43556e9b
Guilherme Reis <guih2127@gmail.com>
2022-10-30 12:21:20 -0300

    More Unit Tests

--- DigimonAppTests/Services/DigimonsServiceTest.cs
+++ DigimonAppTests/Services/DigimonsServiceTest.cs
@@ -4,7 +4,7 @@ using DigimonApp.Services;
 using Moq;
 using Xunit;
 
-namespace DigimonAppTests
+namespace DigimonAppTests.Services
 {
     public class DigimonsServiceTest
     {
@@ -12,7 +12,7 @@ namespace DigimonAppTests
         public Mock<IUnitOfWork> unitOfWork = new Mock<IUnitOfWork>();
 
         [Fact]
-        public async void ListDigimons()
+        public async void ListDigimonsSuccess()
         {
             var digimons = new List<Digimon> {
                 new Digimon { Id = 1, Name = "Name1", Level = "Level1", Image = "Image1" },
@@ -28,7 +28,7 @@ namespace DigimonAppTests
         }
 
         [Fact]
-        public async void SaveDigimon()
+        public async void SaveDigimonSuccess()
         {
             var digimonToInsert = new Digimon { Name = "Name1", Level = "Level1", Image = "Image1" };
 
@@ -40,25 +40,37 @@ namespace DigimonAppTests
         }
 
         [Fact]
-        public async void UpdateDigimon()
+        public async void UpdateDigimonWithSuccess()
         {
             var existingDigimon = new Digimon { Id = 12, Name = "Name1", Level = "Level1", Image = "Image1" };
             digimonsRepositoryMock.Setup(s => s.FindByIdAsync(12)).ReturnsAsync(existingDigimon);
 
-            var digimonUpdated = new Digimon { Id = 12, Name = "Name12", Level = "Level12", Image = "Image12" };
+            var digimonToUpdate = new Digimon { Id = 12, Name = "Name12", Level = "Level12", Image = "Image12" };
 
             var digimonsService = new DigimonsService(digimonsRepositoryMock.Object, unitOfWork.Object);
-            var result = await digimonsService.UpdateAsync(12, digimonUpdated);
+            var result = await digimonsService.UpdateAsync(12, digimonToUpdate);
 
-            Assert.Equal(digimonUpdated.Name, result.Digimon.Name);
-            Assert.Equal(digimonUpdated.Level, result.Digimon.Level);
-            Assert.Equal(digimonUpdated.Image, result.Digimon.Image);
+            Assert.Equal(digimonToUpdate.Name, result.Digimon.Name);
+            Assert.Equal(digimonToUpdate.Level, result.Digimon.Level);
+            Assert.Equal(digimonToUpdate.Image, result.Digimon.Image);
 
             Assert.True(result.Success);
         }
 
         [Fact]
-        public async void DeleteDigimon()
+        public async void UpdateDigimonWithInexistentId()
+        {
+            var digimonToUpdate = new Digimon { Name = "Name12", Level = "Level12", Image = "Image12" };
+
+            var digimonsService = new DigimonsService(digimonsRepositoryMock.Object, unitOfWork.Object);
+            var result = await digimonsService.UpdateAsync(0, digimonToUpdate);
+
+            Assert.Null(result.Digimon);
+            Assert.False(result.Success);
+        }
+
+        [Fact]
+        public async void DeleteDigimonSuccess()
         {
             var digimonToDelete = new Digimon { Id = 1, Name = "Name1", Level = "Level1", Image = "Image1" };
             digimonsRepositoryMock.Setup(s => s.FindByIdAsync(1)).ReturnsAsync(digimonToDelete);
@@ -68,6 +80,16 @@ namespace DigimonAppTests
 
             Assert.Equal(digimonToDelete, result.Digimon);
             Assert.True(result.Success);
+        }
+
+        [Fact]
+        public async void DeleteDigimonWithInexistentId()
+        {
+            var digimonsService = new DigimonsService(digimonsRepositoryMock.Object, unitOfWork.Object);
+            var result = await digimonsService.DeleteAsync(0);
+
+            Assert.Null(result.Digimon);
+            Assert.False(result.Success);
         }
     }
 }
